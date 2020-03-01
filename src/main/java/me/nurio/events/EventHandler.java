d4aa50bd90ad1fc35e8824c2974b0b9b3f2d29e5
xXNurioXx <xxnurioxx@gmail.com>
2020-03-01 02:22:19 +0100

    Created Event Priority enum

--- src/main/java/me/nurio/events/EventHandler.java
+++ src/main/java/me/nurio/events/EventHandler.java
@@ -9,4 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
 
+    EventPriority priority() default EventPriority.NORMAL;
+
 }
---
+++ src/main/java/me/nurio/events/EventPriority.java
@@ -0,0 +1,12 @@
+package me.nurio.events;
+
+public enum EventPriority {
+
+    MONITOR,
+    HIGHEST,
+    HIGH,
+    NORMAL,
+    LOW,
+    LOWEST
+
+}
