f6076a281448c13fe42354c5d6a3a85008fb99a5
xXNurioXx <xxnurioxx@gmail.com>
2020-03-04 18:49:44 +0100

    Refactored EventManager methods in order to sort it better

--- src/main/java/me/nurio/events/EventManager.java
+++ src/main/java/me/nurio/events/EventManager.java
@@ -21,26 +21,26 @@ class EventManagement {
 
     private static Map<Class<?>, List<RegisteredEventListener>> eventMap = new HashMap<>();
 
-    static List<RegisteredEventListener> getEventListenersFor(Event event) {
-        return getEventListenersFor(event.getClass());
-    }
-
-    static List<RegisteredEventListener> getEventListenersOrderedByPriorityFor(Class<?> event){
-        return getEventListenersFor(event);
-    }
-
-    static List<RegisteredEventListener> getEventListenersOrderedByPriorityFor(Event event){
-        return getEventListenersFor(event).stream().sorted(Comparator.comparing(RegisteredEventListener::getPriority)).collect(Collectors.toList());
-    }
-
     static <L extends Listener> void registerEvent(RegisteredEventListener registeredEvent) {
         eventMap.putIfAbsent(registeredEvent.getEvent(), new ArrayList<>());
         getEventListenersFor(registeredEvent.getEvent()).add(registeredEvent);
         System.out.println("Registered event => " + registeredEvent.getName());
     }
 
+    static List<RegisteredEventListener> getEventListenersFor(Event event) {
+        return getEventListenersFor(event.getClass());
+    }
+
     private static List<RegisteredEventListener> getEventListenersFor(Class<?> event) {
         return eventMap.getOrDefault(event, new ArrayList<>());
+    }
+
+    static List<RegisteredEventListener> getEventListenersOrderedByPriorityFor(Event event) {
+        return getEventListenersOrderedByPriorityFor(event.getClass());
+    }
+
+    static List<RegisteredEventListener> getEventListenersOrderedByPriorityFor(Class<?> event) {
+        return getEventListenersFor(event).stream().sorted(Comparator.comparing(RegisteredEventListener::getPriority)).collect(Collectors.toList());
     }
 
 }
