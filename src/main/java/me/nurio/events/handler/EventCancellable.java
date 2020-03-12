0816fc5f9573721100d594f60f68b509341f7f15
xXNurioXx <xxnurioxx@gmail.com>
2020-03-12 01:01:32 +0100

    Added documentation to EventCancellable

--- src/main/java/me/nurio/events/handler/EventCancellable.java
+++ src/main/java/me/nurio/events/handler/EventCancellable.java
@@ -1,9 +1,22 @@
 package me.nurio.events.handler;
 
+/**
+ * This interface will generify the way to cancel events adding the isCancelled() and setCancelled() methods.
+ */
 public interface EventCancellable {
 
+    /**
+     * Validate if the cancellable event was cancelled.
+     *
+     * @return true if cancelled.
+     */
     boolean isCancelled();
 
+    /**
+     * Set the cancellable event as cancelled or not.
+     *
+     * @param cancelled true to set cancelled, false to set uncancelled.
+     */
     void setCancelled(boolean cancelled);
 
 }
