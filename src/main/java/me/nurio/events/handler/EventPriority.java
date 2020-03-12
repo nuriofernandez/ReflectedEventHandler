a27c66668a3ef541e2ee48d91a9a28a0c70e82ef
xXNurioXx <xxnurioxx@gmail.com>
2020-03-12 01:08:02 +0100

    Added documentation to EventPriority

--- src/main/java/me/nurio/events/handler/EventPriority.java
+++ src/main/java/me/nurio/events/handler/EventPriority.java
@@ -1,5 +1,9 @@
 package me.nurio.events.handler;
 
+/**
+ * Event execution order from MONITOR(First) to LOWEST(last).
+ * Order was calculated using the enum ordinal.
+ */
 public enum EventPriority {
 
     MONITOR,
