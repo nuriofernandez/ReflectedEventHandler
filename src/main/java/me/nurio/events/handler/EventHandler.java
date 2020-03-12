d9c4af9242b2f268d7e45f196ea78d47987b1b7e
xXNurioXx <xxnurioxx@gmail.com>
2020-03-12 01:06:31 +0100

    Added documentation to EventHandler

--- src/main/java/me/nurio/events/handler/EventHandler.java
+++ src/main/java/me/nurio/events/handler/EventHandler.java
@@ -5,11 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation will mark methods as Event Handler Listeners.
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
 
+    /**
+     * Mark the event run priority ordered from LOWEST(last) to MONITOR(first).
+     */
     EventPriority priority() default EventPriority.NORMAL;
+
+    /**
+     * Ignore the cancellation of the event an run them inclusive when
+     * it has been cancelled previously by another event handler.
+     */
     boolean ignoreCancelled() default false;
 
 }
