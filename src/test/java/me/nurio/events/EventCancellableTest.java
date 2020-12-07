7d5196f11503ad951715a028323a4a46010535d5
Nurio Fernández <personal@nurio.me>
2020-12-07 02:41:31 +0100

    Moved EventCancellableTest child classes to inner classes

--- src/test/java/me/nurio/events/EventCancellableTest.java
+++ src/test/java/me/nurio/events/EventCancellableTest.java
@@ -1,13 +1,18 @@
 package me.nurio.events;
 
-import me.nurio.events.testclasses.CancellableEvent;
-import me.nurio.events.testclasses.IgnoredCancelledEventTestListener;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import me.nurio.events.handler.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * This class tests the behavior of event execution flow when some of them were cancelled.
+ */
 public class EventCancellableTest {
 
     private EventManager eventManager;
@@ -27,6 +32,37 @@ public class EventCancellableTest {
         // Assert results.
         assertTrue(event.isCancelled());
         assertEquals("CancelledIgnoreCancelled", event.getName());
+    }
+
+    /**
+     * This event listener will be used to test that non 'ignoreCancelled' flagged event handlers were skipped.
+     */
+    private static class IgnoredCancelledEventTestListener implements EventListener {
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void eventCanceller(CancellableEvent eve) {
+            eve.setName("Cancelled");
+            eve.setCancelled(true);
+        }
+
+        @EventHandler(priority = EventPriority.NORMAL)
+        public void noIgnoredEvent(CancellableEvent eve) {
+            eve.setName(eve.getName() + "ThisShouldBeSkipped");
+        }
+
+        @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+        public void ignoredEvent(CancellableEvent eve) {
+            eve.setName(eve.getName() + "IgnoreCancelled");
+        }
+    }
+
+    /**
+     * This event is used to prove event cancellation behaviors.
+     */
+    @Data
+    @RequiredArgsConstructor
+    private static class CancellableEvent extends Event implements EventCancellable {
+        private boolean cancelled;
+        @NonNull private String name;
     }
 
 }
