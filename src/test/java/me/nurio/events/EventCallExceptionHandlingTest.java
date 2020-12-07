2dc79468c7c6bb39d5ead0d988ba08cfaae23712
Nurio Fernández <personal@nurio.me>
2020-12-07 03:13:27 +0100

    Uncopled EventCallExceptionHandlingTest from the rest of test cases

--- src/test/java/me/nurio/events/EventCallExceptionHandlingTest.java
+++ src/test/java/me/nurio/events/EventCallExceptionHandlingTest.java
@@ -1,16 +1,18 @@
 package me.nurio.events;
 
-import me.nurio.events.testclasses.TestEvent;
-import me.nurio.events.testclasses.TestExceptionListener;
-import me.nurio.events.testclasses.TestListener;
+import lombok.Getter;
+import lombok.Setter;
+import me.nurio.events.handler.Event;
+import me.nurio.events.handler.EventHandler;
+import me.nurio.events.handler.EventListener;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-
 import static org.junit.Assert.assertEquals;
 
+/**
+ * This class will test the behavior of an event handler that throws an exception.
+ */
 public class EventCallExceptionHandlingTest {
 
     private EventManager eventManager;
@@ -24,7 +26,7 @@ public class EventCallExceptionHandlingTest {
     public void callingEventHandlerThatThrowExceptions_shouldNotStopTheEventExecutionFlow() {
         // Register event listeners
         eventManager.registerEvents(new TestListener());
-        eventManager.registerEvents(new TestExceptionListener());
+        eventManager.registerEvents(new TestThatThrowsExceptionsListener());
 
         // Create an event and call it
         TestEvent event = new TestEvent();
@@ -32,6 +34,33 @@ public class EventCallExceptionHandlingTest {
 
         // Assert expected behavior
         assertEquals("Changed", event.getTestName());
+    }
+
+    /**
+     * This event listener will be used to prove that in case any other event handler throws an exception this will be executed anyway.
+     */
+    private static class TestListener implements EventListener {
+        @EventHandler
+        public void updateFieldName(TestEvent event) {
+            event.setTestName("Changed");
+        }
+    }
+
+    /**
+     * This event listener will be used to prove the behavior or an event handler that throws an exception.
+     */
+    private static class TestThatThrowsExceptionsListener implements EventListener {
+        @EventHandler
+        public void exceptionThrower(TestEvent event) {
+            throw new RuntimeException("Testing what happens when something goes wrong!");
+        }
+    }
+
+    /**
+     * This event will be used to test the exception throwing behavior.
+     */
+    private static class TestEvent extends Event {
+        @Getter @Setter private String testName;
     }
 
 }
