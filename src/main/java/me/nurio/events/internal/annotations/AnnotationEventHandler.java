78130647fccc8961dd857bdc957295cb06d28124
Nurio Fernández <personal@nurio.me>
2021-06-21 05:03:30 +0200

    (#25) Added documentation to the AnnotationEventHandler interface

--- src/main/java/me/nurio/events/internal/annotations/AnnotationEventHandler.java
+++ src/main/java/me/nurio/events/internal/annotations/AnnotationEventHandler.java
@@ -5,20 +5,50 @@ import me.nurio.events.handler.EventPriority;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
+/**
+ * Represents an annotation event handler.
+ * To create a custom annotation it is required to create a AnnotationEventHandler implementation too.
+ */
 public interface AnnotationEventHandler {
 
+    /**
+     * The annotation class that will be used to handle events with this handler.
+     */
     Class<? extends Annotation> getAnnotation();
 
+    /**
+     * This method should return true in case the requested method matches the
+     * annotation handler criteria. It could be the event type, the annotations
+     * of the method, or any other property.
+     * <p>
+     * In case the method doesn't match the handler criteria, return false to make
+     * the annotation handler manager ignore that method from this handler.
+     *
+     * @param method Method to be handled.
+     * @return 'true' in case this annotation handler matches the method properties.
+     */
     default boolean shouldHandle(Method method) {
         // Default implementation of the handler is to match the annotation.
         return method.getAnnotation(getAnnotation()) != null;
     }
 
+    /**
+     * Should obtain the {@link EventPriority} from reflection method.
+     *
+     * @param method Reflected method to find the EventPriority.
+     * @return EventPriority of the event method.
+     */
     default EventPriority getEventPriority(Method method) {
         // Default implementation in case there is no support for event priorities.
         return EventPriority.NORMAL;
     }
 
+    /**
+     * Should obtain the expected event cancellation behavior from reflection method.
+     *
+     * @param method Reflected method to obtain the expected cancellation behavior.
+     * @return 'true' when it should ignore event cancellations.
+     */
     default boolean shouldIgnoreCancellable(Method method) {
         // Default implementation in case there is no support for event cancellation ignorance.
         return false;
