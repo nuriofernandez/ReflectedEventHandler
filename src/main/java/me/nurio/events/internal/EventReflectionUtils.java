4e6127b5203d6ace11bf7581d7ca939bd5ccb758
Nurio Fernández <personal@nurio.me>
2021-06-21 04:17:49 +0200

    (#25) Removed annotation handling from the EventReflectionUtils class

--- src/main/java/me/nurio/events/internal/EventReflectionUtils.java
+++ src/main/java/me/nurio/events/internal/EventReflectionUtils.java
@@ -2,40 +2,13 @@ package me.nurio.events.internal;
 
 import me.nurio.events.exceptions.EventHandlerNotFoundException;
 import me.nurio.events.handler.Event;
-import me.nurio.events.handler.EventHandler;
-import me.nurio.events.handler.EventPriority;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * This class manages internally event's reflection operations.
  */
 public class EventReflectionUtils {
-
-    /**
-     * Check if specified method its an Event handler.
-     *
-     * @param method Specified method to check.
-     * @return true if its a event handler method.
-     */
-    public static boolean isHandledMethod(Method method) {
-        return method.getAnnotation(EventHandler.class) != null;
-    }
-
-    /**
-     * This method will obtain all methods with the EventHandler annotation.
-     *
-     * @param classListener Listener class handling events.
-     * @return List of reflected methods.
-     */
-    public static List<Method> getHandledMethodsFrom(Class<?> classListener) {
-        return Arrays.stream(classListener.getMethods())
-            .filter(EventReflectionUtils::isHandledMethod)
-            .collect(Collectors.toList());
-    }
 
     /**
      * Obtain event class from reflected method.
@@ -54,28 +27,6 @@ public class EventReflectionUtils {
         }
 
         return (Class<? extends Event>) parameter;
-    }
-
-    /**
-     * Obtain {@link EventPriority} from reflection method.
-     *
-     * @param method Reflected method to find the EventPriority.
-     * @return EventPriority of the event method.
-     */
-    public static EventPriority getEventPriorityFromMethod(Method method) {
-        EventHandler handler = method.getAnnotation(EventHandler.class);
-        return handler.priority();
-    }
-
-    /**
-     * Obtain 'ignoreCancelled' value from reflection method.
-     *
-     * @param method Reflected method to get the 'ignoreCancelled' value.
-     * @return 'true' when ignoreCancelled was enabled.
-     */
-    public static boolean getIgnoreCancelledFromMethod(Method method) {
-        EventHandler handler = method.getAnnotation(EventHandler.class);
-        return handler.ignoreCancelled();
     }
 
 }
