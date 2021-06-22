778a062f6d730fef96ba8bf5504338291fb8debc
Nurio Fernández <personal@nurio.me>
2021-06-23 00:01:20 +0200

    (#29) Externalized EvenManager methods in multiple interfaces

--- src/main/java/me/nurio/events/EventManager.java
+++ src/main/java/me/nurio/events/EventManager.java
@@ -1,18 +1,6 @@
 package me.nurio.events;
 
-import me.nurio.events.handler.Event;
-import me.nurio.events.handler.EventListener;
+import me.nurio.events.internal.eventmanager.ElasticEventManager;
+import me.nurio.events.internal.eventmanager.StandardEventManager;
 
-import java.util.List;
-
-public interface EventManager {
-
-    List<EventListener> getRegisteredListeners();
-
-    <L extends EventListener> void registerEvents(L listener);
-
-    <L extends EventListener> void unregisterEvents(L listener);
-
-    <E extends Event> void callEvent(E event);
-
-}
+public interface EventManager extends StandardEventManager, ElasticEventManager {}
